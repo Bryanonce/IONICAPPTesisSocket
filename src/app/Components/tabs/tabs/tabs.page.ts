86cd21ed1540689a73ce5aa88b48fe88b94611f6
Bryan <bradcede@espol.edu.ec>
2020-12-17 17:04:35 -0500

    Añadiendo Filtro de Kalman al GPS

--- src/app/Components/tabs/tabs/tabs.page.ts
+++ src/app/Components/tabs/tabs/tabs.page.ts
@@ -25,28 +25,27 @@ import { MetodoPost } from '../../../module/post.module';
   styleUrls: ['tabs.page.scss']
 })
 export class TabsPage {
-  public ultimaMedicion:EntragaModule = new EntragaModule();
-  public medicionesMax:Number = 50;
-  public contador:number = 0;
-  public inicio:Boolean;
-  constructor(public _geolocation:GeolocalService,
-              public _servicioService:ServicioService,
-              public _webSocket:WebSocketService,
-              private localNotifications: LocalNotifications,
-              private storage:Storage,
-              public router: Router
-              ) 
-  {
+  public ultimaMedicion: EntragaModule = new EntragaModule();
+  public medicionesMax: Number = 50;
+  public contador: number = 0;
+  public inicio: Boolean;
+  constructor(public _geolocation: GeolocalService,
+    public _servicioService: ServicioService,
+    public _webSocket: WebSocketService,
+    private localNotifications: LocalNotifications,
+    private storage: Storage,
+    public router: Router
+  ) {
     this._webSocket.revisarStatus(true);
-    this.storage.ready().then(()=>{
+    this.storage.ready().then(() => {
       this.storage.get('userId')
-        .then((userId:string) =>{
+        .then((userId: string) => {
           this._webSocket.listendEvent('avisoPeligro')
-            .subscribe((data:{id:string,peligro:boolean})=>{
-              if(data.id === userId && data.peligro === true){
-                if(!environment.production){
+            .subscribe((data: { id: string, peligro: boolean }) => {
+              if (data.id === userId && data.peligro === true) {
+                if (!environment.production) {
                   alert('Peligro inminente');
-                }else{
+                } else {
                   this.llamarNoti();
                 }
               }
@@ -54,63 +53,73 @@ export class TabsPage {
         })
     })
   }
-  
-  llamarNoti(){
+
+  llamarNoti() {
     let isAndroid = true;
     this.localNotifications.schedule({
       id: 1,
       text: 'Es probable que se encuentre en una zona de Peligro',
-      sound: isAndroid? 'file://sound.mp3': 'file://beep.caf',
+      sound: isAndroid ? 'file://sound.mp3' : 'file://beep.caf',
       data: { secret: 'Hola' }
     });
   }
 
-  validacionServicio(){
-    if(this.inicio==true){
+  validacionServicio() {
+    if (this.inicio == true) {
       this.inicio = false;
-    }else{
+    } else {
       this.inicio = true;
       this.buclePost()
     }
   }
 
-  async buclePost(){
-    do{
-      this.contador++
-      await this.contadorPromesa()
-       .then((resp)=>{
-        this._geolocation.suscribirPost()
-          .then((response:MetodoPost)=>{
-            //console.log(response);
-             this._webSocket.emitirMsj('enviarCoordServ',response)
-          })
-      }).catch((err)=>{
-        console.log(err)
-      })
-    }while(this.inicio===true)
+  async buclePost() {
+    let dataCoor: MetodoPost[] = [];
+    let count = 0
+    let numArray:number = 5
+    do {
+      if (count < numArray) {
+      } else {
+        count = 0;
+        dataCoor.splice(0,dataCoor.length);
+      }
+      this.contador++;
+      count++;
+      await this._geolocation.suscribirPost()
+        .then((res: MetodoPost) => {
+          dataCoor.push(res);
+          return this.contadorPromesa(500);
+        }).then(() => {
+          //console.log(dataCoor);
+          if(dataCoor.length === numArray){
+            this._webSocket.emitirMsj('enviarCoordServ',dataCoor)
+          }
+        })
+      
+    } while (this.inicio === true /*&& this.contador <= 5*/)
     this.inicio = false;
     console.log('Ha salido del bucle');
     this.contador = 0;
   }
 
-  contadorPromesa(){
-    return new Promise((resolve,reject)=>{
-      setTimeout(() =>{
+  contadorPromesa(time: number) {
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
         //console.log('Esperando ....');
         resolve(true);
-      },10000)
-      
+      }, time)
+
     })
   }
 
-  cerrarSesion(){
+  cerrarSesion() {
     this.storage.remove('googleAuth')
-    .then(()=>{
-      return this.storage.remove('userId')
-    })
-    .then(()=>{
-      this.router.navigate(['/']);
-    })
+      .then(() => {
+        return this.storage.remove('userId')
+      })
+      .then(() => {
+        this.router.navigate(['/']);
+      })
   }
 
 }
